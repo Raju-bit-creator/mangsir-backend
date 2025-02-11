9ced0a8007b3f048ab267d6cca0832b8d6c22092
Raju <rajutamang3456@gmail.com>
2025-02-11 14:54:59 +0545

    chat data

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { chats } = require("./data/data");
 const app = express();
 let PORT = process.env.PORT || 5000; //assignment research about MVC architecture
 // console.log(PORT);
@@ -8,6 +9,16 @@ app.get("/", function (req, res) {
 });
 app.get("/mangsir-group", function (req, res) {
   res.send("frontend completed");
+});
+app.get("/chats", function (req, res) {
+  res.json(chats);
+});
+app.get("/chats/:id", function (req, res) {
+  console.log(req);
+
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  // console.log(singleChat);
+  res.json(singleChat);
 });
 app.listen(PORT, () => {
   console.log(`api is running on port ${PORT}`);
